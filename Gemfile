ad2b2a0b491f05ec6daaf6ad820b695799ddc8da
Ronnie Taylor <ronnie@ronnietaylor.com>
2013-06-09 21:10:19 -0700

    added sinatra asset pack. added partials. split header and footer into partial.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'http://rubygems.org'
 gem 'sinatra', '1.4.3'
+gem 'sinatra-partial', '0.4.0'
+gem 'sinatra-assetpack'
 gem 'thin', '1.5.1'
 gem 'slim', '1.3.8'
 gem 'sass', '3.2.6'
--- backbone.rb
+++ backbone.rb
@@ -1,5 +1,7 @@
 # Get ready to kick ass.
 require 'sinatra'
+require 'sinatra/partial'
+require 'sinatra/assetpack'
 require 'slim'
 require 'sass'
 require 'compass'
@@ -8,14 +10,39 @@ require 'zurb-foundation'
 configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.config'))
   set :template_engine, :slim
+  set :partial_template_engine, :slim
+  enable :partial_underscores
   set :format => :html5
+  set :root, File.dirname(__FILE__)
+end
+
+configure :production, :development do
+  enable :logging
 end
 
 class Backbone < Sinatra::Base
+  register Sinatra::AssetPack
+  register Sinatra::Partial
 
-  configure :production, :development do
-    enable :logging
-  end
+  assets {
+    serve '/js',      from: 'assets/js'
+    serve '/css',     from: 'assets/css'
+    serve '/images',  from: 'assets/images'
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    js :app, '/js/app.js', [
+      '/js/vendor/**/*.js',
+      '/js/lib/**/*.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/screen.css'
+    ]
+
+    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
+    css_compression :sass   # :simple | :sass | :yui | :sqwish
+  }
 
   get '/' do
    slim :index
